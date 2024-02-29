3912c944a04413cfb5bf9af11693b47277acb93c
Arpit-Verma-2003 <arpitverma200316@gmail.com>
2024-03-01 00:27:45 +0530

    added fucntionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import {useState} from 'react';
 import CoreComponents from "./Components/CoreComponents.jsx";
 import { CORE_CONCEPTS } from "./data";
 import Header from "./Components/Header.jsx";
 import TabButton from "./Components/TabButton.jsx";
-function clickHandler(selectedButton){
-  console.log(selectedButton);
-}
+import { EXAMPLES } from './data';
+
 function App() {
+  const [selectedTopic,setSelectedTopic] = useState('');
+  function clickHandler(selectedButton){
+    setSelectedTopic(selectedButton);
+    console.log({selectedTopic});
+  }
+  let tabContent = <p>Please select a topic</p>
+  if(selectedTopic){
+    tabContent = (<div id='tab-content'>
+    <h3>{EXAMPLES[selectedTopic].title}</h3>
+    <p>{EXAMPLES[selectedTopic].description}</p>
+    <pre>
+      <code>{EXAMPLES[selectedTopic].code}</code>
+    </pre>
+  </div>);
+  }
   return (
     <div>
       <main>
@@ -31,11 +46,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onPress = {()=>clickHandler(1)}>Click it</TabButton>
-            <TabButton onPress = {()=>clickHandler(2)}>yoyo</TabButton>
-            <TabButton onPress = {()=>clickHandler(3)}>aji ha</TabButton>
-            <TabButton onPress = {()=>clickHandler(4)}>launde</TabButton>
+            <TabButton onPress = {()=>clickHandler('components')}>Click it</TabButton>
+            <TabButton onPress = {()=>clickHandler('jsx')}>yoyo</TabButton>
+            <TabButton onPress = {()=>clickHandler('props')}>aji ha</TabButton>
+            <TabButton onPress = {()=>clickHandler('state')}>launde</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
